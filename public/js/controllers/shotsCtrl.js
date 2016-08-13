cd793073d741b582e2a0e557bc2a0428fa992595
rcmaiolini <ricardo.maiolini@gmail.com>
2016-08-13 15:33:54 -0300

    Inicializacao do angular + controller principal

---
+++ public/js/controllers/shotsCtrl.js
@@ -0,0 +1,18 @@
+app.controller('shotsCtrl', ['$scope', 'shotsApi' , function($scope, shotsApi){
+
+  $scope.shots = [];
+
+  shotsApi.query(function(shots) {
+		$scope.shots = shots;
+    $('.materialboxed').materialbox();
+    console.log(shots);
+	}, function(erro) {
+		console.log(erro);
+	});
+
+  $scope.ordenarPor = function(campo){
+    console.log(campo);
+    $scope.criterioDeOrdenacao = campo;
+    $scope.direcaoDaOrdenacao = !$scope.direcaoDaOrdenacao;
+  };
+}]);
