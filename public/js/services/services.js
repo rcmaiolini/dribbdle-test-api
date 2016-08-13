b21ccbce49932d0325cf4beb2fdb7014308b2f35
rcmaiolini <ricardo.maiolini@gmail.com>
2016-08-13 15:35:13 -0300

    Service com api do dribbble

---
+++ public/js/services/services.js
@@ -0,0 +1,7 @@
+app.factory('shotsApi', ['$resource', function($resource) {
+	var apiUrl = 'https://api.dribbble.com/v1';
+	var token = '?access_token=b6e52eda90d048997dc4469be17b975d3d35167c9d23eed5b486fac91b2b2b74&page=2&per_page=30';
+
+	return $resource(apiUrl + '/shots' + token);
+
+}]);
