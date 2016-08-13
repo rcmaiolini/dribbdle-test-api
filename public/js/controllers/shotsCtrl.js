02d8921a78b38346f703c37e12d26f89cf74137c
rcmaiolini <ricardo.maiolini@gmail.com>
2016-08-13 18:16:25 -0300

    colocando paginacao

--- public/js/controllers/shotsCtrl.js
+++ public/js/controllers/shotsCtrl.js
@@ -1,11 +1,12 @@
 app.controller('shotsCtrl', ['$scope', 'shotsApi' , function($scope, shotsApi){
 
-  $scope.shots = [];
+	$scope.shots = [];
 
-  shotsApi.query(function(shots) {
+	shotsApi.query(function(shots) {
 		$scope.shots = shots;
-    $('.materialboxed').materialbox();
 	}, function(erro) {
 		console.log(erro);
 	});
+
+
 }]);
--- public/js/services/services.js
+++ public/js/services/services.js
@@ -1,8 +1,7 @@
 app.factory('shotsApi', ['$resource', function($resource) {
 	var apiUrl = 'https://api.dribbble.com/v1';
-	var token = '?access_token=b6e52eda90d048997dc4469be17b975d3d35167c9d23eed5b486fac91b2b2b74';
-	var pages = '&page=20&per_page=12';
+	var token = '?access_token=b6e52eda90d048997dc4469be17b975d3d35167c9d23eed5b486fac91b2b2b74&page=1&per_page=28';
 
-	return $resource(apiUrl + '/shots' + token + pages);
+	return $resource(apiUrl + '/shots' + token);
 
 }]);
