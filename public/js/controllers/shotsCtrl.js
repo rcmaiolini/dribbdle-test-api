52f00eb605b1eaf713fe75f4fc887ecdab9d0542
rcmaiolini <ricardo.maiolini@gmail.com>
2016-08-13 18:57:20 -0300

    Implementando ng-simplePagination

--- public/js/controllers/shotsCtrl.js
+++ public/js/controllers/shotsCtrl.js
@@ -1,9 +1,11 @@
-app.controller('shotsCtrl', ['$scope', 'shotsApi' , function($scope, shotsApi){
+app.controller('shotsCtrl', ['$scope', 'shotsApi', 'Pagination' , function($scope, shotsApi, Pagination){
 
 	$scope.shots = [];
 
 	shotsApi.query(function(shots) {
 		$scope.shots = shots;
+		$scope.pagination = Pagination.getNew(12);
+		$scope.pagination.numPages = Math.ceil($scope.shots.length/$scope.pagination.perPage);
 	}, function(erro) {
 		console.log(erro);
 	});
--- public/js/services/services.js
+++ public/js/services/services.js
@@ -1,6 +1,6 @@
 app.factory('shotsApi', ['$resource', function($resource) {
 	var apiUrl = 'https://api.dribbble.com/v1';
-	var token = '?access_token=b6e52eda90d048997dc4469be17b975d3d35167c9d23eed5b486fac91b2b2b74&page=1&per_page=28';
+	var token = '?access_token=b6e52eda90d048997dc4469be17b975d3d35167c9d23eed5b486fac91b2b2b74&page=1&per_page=100';
 
 	return $resource(apiUrl + '/shots' + token);
 
